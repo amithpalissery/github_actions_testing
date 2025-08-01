20eddb828f68ec7d20be45dc76964029f034b46c
Amith P <107483173+amithpalissery@users.noreply.github.com>
2025-08-01 15:18:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Node.js App! Version 1.1');
+  res.send('Hello from the Node.js App! Version 1.0');
 });
 
 app.listen(port, () => {
